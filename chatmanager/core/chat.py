5082c8752a3a801411868d68a67809a0a137c3ab
Peiwei Hu <jlu.hpw@foxmail.com>
2023-09-09 17:44:09 +0800

    Implementation of three core classes in session.py
    * ChatMessage for constructing queries
    * ChatResponse for parsing response from LLM
    * Session for storing the interaction

--- chatmanager/core/chat.py
+++ chatmanager/core/chat.py
@@ -1,17 +1,79 @@
-from typing import Optional, List
+from typing import Optional, List, Dict, Tuple
+from dataclasses import dataclass
 
 import openai
 
-from .session import Session
+from .session import Session, ChatMessage, ChatResponse
 from .key import KeyGroup
 
 
-def send_msg(messages) -> str:
-    response = openai.Completion.create(
-        model = "gpt-3.5-turbo",
-        messages = messages,
+@dataclass
+class ChatSetup:
+    """ Setup openai interface
+
+    """
+
+    model: str = "gpt-3.5-turbo"
+    base: str = "https://api.openai.com"
+    key: Optional[str] = None
+
+
+"""
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+{
+  "id": "chatcmpl-123",
+  "object": "chat.completion",
+  "created": 1677652288,
+  "model": "gpt-3.5-turbo-0613",
+  "choices": [{
+    "index": 0,
+    "message": {
+      "role": "assistant",
+      "content": "\n\nHello there, how may I assist you today?",
+    },
+    "finish_reason": "stop"
+  }],
+  "usage": {
+    "prompt_tokens": 9,
+    "completion_tokens": 12,
+    "total_tokens": 21
+  }
+}
+
+"""
+
+
+def send_msg(msg: List[Dict[str, str]], setup: ChatSetup) -> ChatResponse:
+    """Send a message to openai
+
+    Args:
+        msg: A list of messages
+        setup: The ChatSetup object
+
+    Returns:
+        The ChatCompletion object
+
+    """
+
+    openai.base = setup.base
+    openai.api_key = setup.key
+
+    #TODO different parameters
+    response = openai.ChatCompletion.create(
+        model=setup.model,
+        messages=msg,
     )
-    return response
+
+    return ChatResponse(response)
 
 
 class ChatManager:
@@ -22,9 +84,102 @@ class ChatManager:
         sessions: A list of all sessions
         keys: A KeyGroup object managing key-related stuff
 
+    Methods:
+        set_session: Set the current session
+
     """
 
     def __init__(self) -> None:
         self.cur_session: Optional[Session] = None
         self.sessions: List[Session] = []
         self.keys: KeyGroup = KeyGroup()
+        self.setup: ChatSetup = ChatSetup()
+
+    def is_ready(self) -> bool:
+        """Check if the ChatManager is ready to work
+
+        TODO: Add hints for reasons of failure
+        """
+
+        # key check
+        if self.setup.key is None:
+            return False
+
+        # cur session check
+        if self.cur_session is None:
+            return False
+
+        return True
+
+    def send(self, msg: ChatMessage) -> Optional[ChatResponse]:
+        """Send a message to openai
+
+        Args:
+            msg: The message to send
+
+        Returns:
+            ChatResponse if the ChatManager is ready, None otherwise
+
+        """
+
+        if not self.is_ready():
+            # TODO: throw error
+            return None
+
+        return send_msg(msg.drain(), self.setup)
+
+
+    def set_session(self, name: str) -> None:
+        """Assign the current session
+
+        If the session does not exist, create a new one.
+
+        Args:
+            name: The name of the session
+        """
+
+        session = self.get_session(name)
+        if not session:
+            self.sessions.append(session := Session(name))
+        self.cur_session = session
+
+    def get_session(self, name: str) -> Optional[Session]:
+        """Get a session by name
+
+        Args:
+            name: The name of the session
+
+        Returns:
+            The session if it exists, None otherwise
+
+        """
+
+        for session in self.sessions:
+            if session.name == name:
+                return session
+        return None
+
+    def export_session(self, name: str, export_path: str, format: str = 'json') -> bool:
+        """Export the current session
+
+        Args:
+            name: The name of the session
+            format: The format of the export file
+
+        Returns:
+            True if the export is successful, False otherwise
+
+        """
+
+        if not (session := self.get_session(name)):
+            # TODO: throw error
+            return False
+
+        handlers = {}
+
+        if not (handler := handlers.get(format)):
+            # TODO: throw error
+            return False
+
+        handler(export_path)
+        return True
--- chatmanager/core/session.py
+++ chatmanager/core/session.py
@@ -1,51 +1,92 @@
 import json
 import os
-from typing import Dict
+from typing import Dict, Optional, List, Callable, Tuple
 
 
-class Session:
-    """Store a session with ChatGPT
+class ChatMessage:
+    """Construct the message for single interaction
 
     Attributes:
-        name: the name of this session
-        repo: store the messages, order sensitive, {index: {role: message}}
-
+        repo: store the messages, order sensitive
     """
 
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.repo: Dict[int, Dict[str, str]] = dict()
+    def __init__(self) -> None:
+        self.repo: List[Dict[str, str]] = list()
 
     def set_repo(self, index: int, message: Dict[str, str]) -> None:
         self.repo[index] = message
 
-    def repo_next_index(self) -> int:
-        ks = self.repo.keys()
-        if not ks:
-            return 0
-        return max(ks) + 1
+    def push_system(self, msg: str) -> None:
+        self.push_msg({
+            "role": "system",
+            "message": msg
+        })
 
-    def set_system(self, system_prompt: str) -> None:
-        system = {
-            'role': 'system',
-            'content': system_prompt,
-        }
-        self.set_repo(0, system)
+    def push_user(self, msg: str) -> None:
+        self.push_msg({
+            "role": "user",
+            "message": msg
+        })
 
-    def push_message(self, message: Dict[str, str]) -> None:
-        next_index = self.repo_next_index()
-        self.set_repo(next_index, message)
+    def push_assistant(self, msg: str) -> None:
+        self.push_msg({
+            "role": "assistant",
+            "message": msg
+        })
 
-    def export_json(self, jpath: str) -> None:
-        assert (not os.path.exists(jpath))
+    def push_msg(self, message: Dict[str, str]) -> None:
+        self.repo.append(message)
 
-        with open(jpath, 'w') as w:
-            json.dump(self.repo, w, indent=4)
+    def del_msg(self, delete_checker: Callable[[Dict[str, str]], bool]) -> None:
+        """ Delete the entries that satisfy delete_checker (i.e. return True) """
 
-    def load_json(self, jpath: str) -> None:
-        """ construct new repo by json """
+        self.repo = list(filter(lambda x: not delete_checker(x), self.repo))
 
-        assert (os.path.exists(jpath))
+    def drain(self) -> List[Dict[str, str]]:
+        return self.repo
 
-        with open(jpath, 'r') as r:
-            self.repo = {int(k): v for k, v in json.load(r).items()}
+
+class ChatResponse:
+    """Parse the response from openai
+
+    Attributes:
+        response: The response from openai
+
+    Methods:
+        token_usage: Get the number of tokens used
+        choice_num: Get the number of choices
+        get_msg: Get the message of a choice
+    """
+
+    def __init__(self, response) -> None:
+        self.response = response
+
+    def token_usage(self) -> int:
+        return self.response["usage"]["total_tokens"]
+
+    def choice_num(self) -> int:
+        return len(self.response["choices"])
+
+    def get_msg(self, choice:int = 0) -> Optional[str]:
+        if choice >= self.choice_num():
+            return None
+
+        return self.response["choices"][choice]["message"]["content"]
+
+
+class Session:
+    """Log the interaction between user and openai
+
+    Store each message and response in a list
+
+    Attributes:
+        name: The name of the session
+        repo: The list of (message, response) tuples
+    """
+
+    def __init__(self, name: str) -> None:
+        self.name: str = name
+        self.repo: List[Tuple[ChatMessage, ChatResponse]] = list()
+
+    def push(self, msg: ChatMessage, response: ChatResponse) -> None:
+        self.repo.append((msg, response))
